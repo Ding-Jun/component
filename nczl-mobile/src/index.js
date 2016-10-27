2b6ce7661384f1eeedfcc837b2bc5e9326900913
funtest <85182404@qq.com>
2016-10-27 20:11:23 +0800

    good night

---
+++ nczl-mobile/src/index.js
@@ -0,0 +1,8 @@
+
+/**
+ * Created by admin on 2016/10/27.
+ */
+import './js/index'
+
+
+console.log("mmmmm back2")
--- nczl-mobile/src/js/index.js
+++ nczl-mobile/src/js/index.js
@@ -1,5 +1,30 @@
-//兼容IE8 add es5-polyfill  size:33K
-import 'es5-shim';
-import 'es5-shim/es5-sham';
-import 'console-polyfill';
+
 import 'core-js/fn/object/assign';
+import urlEncode from './common/urlEncode'
+import {root} from './common/constants'
+$(function(){
+  showArticleList(1,{columnId:5});
+  console.log('String fiy',urlEncode({}));
+})
+
+
+function showArticleList(targetPage,query) {
+
+  var queryString=urlEncode(query);
+  var url=`${root}/rs/article/list?curPage=${targetPage}&pageSize=10${queryString}`;
+  $.ajax({
+    type: 'GET',
+    url: url,
+    dataType: 'json',
+    success: function (rm) {
+      if (rm.code == 1) {
+        doshowArticleList(rm.result);
+      }
+    }
+  })
+
+}
+
+function doshowArticleList(page) {
+
+}
