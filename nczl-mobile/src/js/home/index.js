2c718b003091141871a66ff66f1ed32241fb0a56
DESKTOP-RQLJO7D\a8518 <851824004@qq.com>
2016-10-31 00:47:46 +0800

    webpack complete2

--- nczl-mobile/src/js/home/index.js
+++ nczl-mobile/src/js/home/index.js
@@ -1,33 +1,31 @@
-
 import 'core-js/fn/object/assign';
 import {map} from 'lodash'
 import $ from 'jquery'
 import urlEncode from '../common/urlEncode'
 import {root} from '../common/constants'
-import {showArticleList,doShowArticleList} from '../article/article'
+import {showArticleList, doShowArticleList} from '../article/article'
+/*
+ $(function(){
+ //showArticleList(1,{columnId:5});
+ //showArticleList(1)
+ doShowArticleList([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]);
+ $('#searchForm').bind('submit',function (e) {
+ e.preventDefault();
+ window.location.href =`search.html?keyword=${encodeURIComponent($('#searchForm > .si').val())}`;
+ console.log("youyyyy sumt",$('#searchForm > .si').val())
+ })
 
-$(function(){
-  //showArticleList(1,{columnId:5});
-  //showArticleList(1)
-  doShowArticleList([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]);
-  $('#searchForm').bind('submit',function (e) {
-    e.preventDefault();
-    window.location.href =`search.html?keyword=${encodeURIComponent($('#searchForm > .si').val())}`;
-    console.log("youyyyy sumt",$('#searchForm > .si').val())
-  })
+ var myScroll= new IScroll('#wrapper',{
+ mouseWheel: true,
+ tap:true
+ });
 
-  var myScroll= new IScroll('#wrapper',{
-    mouseWheel: true,
-    tap:true
-  });
+ doShowArticleList([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]);
+ setTimeout(function () { myScroll.refresh(); }, 0);
+ })
 
-  doShowArticleList([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]);
-  setTimeout(function () { myScroll.refresh(); }, 0);
-})
-
-document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
-
-
+ document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+ */
 
 
 var myScroll,
@@ -44,14 +42,14 @@ var myPullUpRefrushLabel = '松手开始更新...';
 var myPullDownLabel = '下拉刷新...';
 var myPullDownRefushLabel = '松手开始更新...';
 var myPullLoadLabel = '加载中...';
-function pullDownAction () {
-  appendNewReviewsList();
-
+function pullDownAction() {
+  ///appendNewReviewsList();
+  console.log("pullDownAction")
 }
 
-function pullUpAction () {
-  appendNewReviewsList();
-
+function pullUpAction() {
+  //appendNewReviewsList();
+  console.log("pullUpAction")
 }
 
 /**
@@ -63,11 +61,12 @@ function loaded() {
   pullUpEl = document.getElementById('pullUp');
   pullUpOffset = pullUpEl.offsetHeight;
 
-  myScroll = new iScroll('wrapper', {
+  myScroll = new IScroll('#wrapper', {
     scrollbarClass: 'myScrollbar', /* 重要样式 */
     useTransition: false, /* 此属性不知用意，本人从true改为false */
     topOffset: pullDownOffset,
     onRefresh: function () {
+      console.log("onRefresh")
       if (pullDownEl.className.match('loading')) {
         pullDownEl.className = '';
         pullDownEl.querySelector('.pullDownLabel').innerHTML = myPullDownLabel;
@@ -77,6 +76,7 @@ function loaded() {
       }
     },
     onScrollMove: function () {
+      console.log("onScrollMove")
       if (this.y > 5 && !pullDownEl.className.match('flip')) {
         pullDownEl.className = 'flip';
         pullDownEl.querySelector('.pullDownLabel').innerHTML = '';
@@ -96,6 +96,7 @@ function loaded() {
       }
     },
     onScrollEnd: function () {
+      console.log("onScrollEnd")
       if (pullDownEl.className.match('flip')) {
         pullDownEl.className = 'loading';
         pullDownEl.querySelector('.pullDownLabel').innerHTML = myPullLoadLabel;
@@ -108,170 +109,185 @@ function loaded() {
     }
   });
 
-  setTimeout(function () { document.getElementById('wrapper').style.left = '0'; }, 800);
+  setTimeout(function () {
+    document.getElementById('wrapper').style.left = '0';
+  }, 800);
+  myScroll.on('scroll', ()=>{console.log("scroll")});
+  myScroll.on('scrollEnd', ()=>{console.log("scrollEnd")});
 }
 
 //初始化绑定iScroll控件
-document.addEventListener('touchmove', function (e) { $(".commont")||e.preventDefault(); }, false);
+document.addEventListener('touchmove', function (e) {
+  $(".commont") || e.preventDefault();
+}, false);
 document.addEventListener('DOMContentLoaded', loaded, false);
 
-  $(function(){
-  $( ".reviews" ).show();
-  reloadReviewsList();
+$(function () {
+  doShowArticleList([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]);
+  $('#searchForm').bind('submit',function (e) {
+    e.preventDefault();
+    window.location.href =`search.html?keyword=${encodeURIComponent($('#searchForm > .si').val())}`;
+    console.log("youyyyy sumt",$('#searchForm > .si').val())
+  })
+  $(".reviews").show();
+  console.log("reloadReviewsList")
+  setTimeout(function () { myScroll.refresh(); }, 1000);
+  // reloadReviewsList();
 })
-  var lastReviewId = null;
-  function isAndroid(){
+var lastReviewId = null;
+function isAndroid() {
   return /Android/.test(navigator.userAgent);
 }
-  function setupWebViewJavascriptBridge(callback) {
+function setupWebViewJavascriptBridge(callback) {
   if (window.WebViewJavascriptBridge) {
-  return callback(WebViewJavascriptBridge);
-}
+    return callback(WebViewJavascriptBridge);
+  }
   if (window.WVJBCallbacks) {
-  return window.WVJBCallbacks.push(callback);
-}
+    return window.WVJBCallbacks.push(callback);
+  }
   window.WVJBCallbacks = [callback];
   var WVJBIframe = document.createElement('iframe');
   WVJBIframe.style.display = 'none';
   WVJBIframe.src = 'wvjbscheme://__BRIDGE_LOADED__';
   document.documentElement.appendChild(WVJBIframe);
-  setTimeout(function() {
-  document.documentElement.removeChild(WVJBIframe)
-}, 0)
+  setTimeout(function () {
+    document.documentElement.removeChild(WVJBIframe)
+  }, 0)
 }
-  function showReviews(){
+function showReviews() {
   var jsonData;
-  if(isAndroid()){
-  jsonData = window.o2Android.getGoodsInfo();
-  if(typeof jsonData == "string"){
-  jsonData = eval('('+jsonData+')');
-  $( "#userId" ).val( jsonData.user_id );
-  $( "#productId" ).val( jsonData.product_id );
-  $("#colCityCode").val( jsonData.city_code );
-  console.info( 'colIdItem:' + jsonData.product_id );
-  console.info('colCityCode:'+jsonData.city_code);
+  if (isAndroid()) {
+    jsonData = window.o2Android.getGoodsInfo();
+    if (typeof jsonData == "string") {
+      jsonData = eval('(' + jsonData + ')');
+      $("#userId").val(jsonData.user_id);
+      $("#productId").val(jsonData.product_id);
+      $("#colCityCode").val(jsonData.city_code);
+      console.info('colIdItem:' + jsonData.product_id);
+      console.info('colCityCode:' + jsonData.city_code);
 
-  $( ".reviews" ).show();
-  reloadReviewsList();
-}
-}else{
-  setupWebViewJavascriptBridge(function(bridge){});
-  var bridge = window.WebViewJavascriptBridge;
-  if (typeof (bridge.callHandler) == 'function'){
-  bridge.callHandler('getGoodInfo', {'': ''}, function(response){
-  jsonData = eval('('+response+')');
-  $( "#userId" ).val( jsonData.user_id );
-  $( "#productId" ).val( jsonData.product_id );
-  $("#colCityCode").val( jsonData.city_code );
-  console.info( 'colIdItem:' + jsonData.product_id );
-  console.info('colCityCode:'+jsonData.city_code);
+      $(".reviews").show();
+      reloadReviewsList();
+    }
+  } else {
+    setupWebViewJavascriptBridge(function (bridge) {
+    });
+    var bridge = window.WebViewJavascriptBridge;
+    if (typeof (bridge.callHandler) == 'function') {
+      bridge.callHandler('getGoodInfo', {'': ''}, function (response) {
+        jsonData = eval('(' + response + ')');
+        $("#userId").val(jsonData.user_id);
+        $("#productId").val(jsonData.product_id);
+        $("#colCityCode").val(jsonData.city_code);
+        console.info('colIdItem:' + jsonData.product_id);
+        console.info('colCityCode:' + jsonData.city_code);
 
-  $( ".reviews" ).show();
-  reloadReviewsList();
-})
-}
-}
+        $(".reviews").show();
+        reloadReviewsList();
+      })
+    }
+  }
 }
 
-  function appendReviewsList ( reviewList ) {
+function appendReviewsList(reviewList) {
   var listElms = '';
 
-  if ( reviewList == null || reviewList.length <= 0 ) {
-  return '';
-}
+  if (reviewList == null || reviewList.length <= 0) {
+    return '';
+  }
 
-  $.each( reviewList, function ( i, item ) {
-  var userName = ( item.user_name != null ? item.user_name : '' );
-  var time = ( item.comment_date != null ? item.comment_date : '' );
-  var stars = getStars( item.product_score );
-  var cnt = ( item.comment_content != null ? item.comment_content : '' );
+  $.each(reviewList, function (i, item) {
+    var userName = ( item.user_name != null ? item.user_name : '' );
+    var time = ( item.comment_date != null ? item.comment_date : '' );
+    var stars = getStars(item.product_score);
+    var cnt = ( item.comment_content != null ? item.comment_content : '' );
 
-  listElms += '<li><div class="cltop"><span>' + userName + ' ' + time + '</span>' + stars + '</div>' + cnt + '</li>';
+    listElms += '<li><div class="cltop"><span>' + userName + ' ' + time + '</span>' + stars + '</div>' + cnt + '</li>';
 
-  lastReviewId = item.id;
-} );
-  $( "#oldestId" ).val( lastReviewId );
-  $( "#thelist" ).append( listElms );
+    lastReviewId = item.id;
+  });
+  $("#oldestId").val(lastReviewId);
+  $("#thelist").append(listElms);
   myScroll.refresh();
 }
 
-  function getStars ( stars ) {
+function getStars(stars) {
   var starElms = '';
 
-  if ( stars != null ) {
-  while ( stars-- > 0 ) {
-  starElms += '<img src="http://120.27.163.126/templateSource/icon_star.png">';
-}
-}
+  if (stars != null) {
+    while (stars-- > 0) {
+      starElms += '<img src="http://120.27.163.126/templateSource/icon_star.png">';
+    }
+  }
 
   return starElms;
 }
 
-  var lastComments = 0;
-  var pageNum = 0;
-  function reloadReviewsList () {
-  $( "#thelist" ).children().remove();
-  $( "#oldestId" ).val( '' );
-  $( ".pullUpLabel" ).html( '上拉加载更多...' );
-  var formParams = $( "#prodInfoCommentsForm" ).serialize();
+var lastComments = 0;
+var pageNum = 0;
+function reloadReviewsList() {
+  $("#thelist").children().remove();
+  $("#oldestId").val('');
+  $(".pullUpLabel").html('上拉加载更多...');
+  var formParams = $("#prodInfoCommentsForm").serialize();
   pageNum = 0;
-  $.ajax( {
-  type : 'GET',
-  url : 'http://114.55.43.64:8080/o2reviews/product/productinfocomments',
-  data : formParams,
-  dataType : 'jsonp',
-  contentType : 'application/json'
-} );
+  $.ajax({
+    type: 'GET',
+    url: 'http://114.55.43.64:8080/o2reviews/product/productinfocomments',
+    data: formParams,
+    dataType: 'jsonp',
+    contentType: 'application/json'
+  });
 }
-  function prodInfoComments ( data ) {
+function prodInfoComments(data) {
   pageNum += 1;
-  if ( data.code == 1 ) {
-  lastComments = data.result.length;
-  appendReviewsList( data.result );
-  if ( lastComments != 15 ) {
-  if (pageNum != 1) {
-  myPullLoadLabel = '评论已经加载完啦';
-  myPullUpLabel = '评论已经加载完啦';
-  myPullUpRefrushLabel = '评论已经加载完啦';
-  $("#pull_up_icon").removeClass();
-  $( ".pullUpLabel" ).html( '评论已经加载完啦' );
-} else {
-  myPullLoadLabel = '';
-  myPullUpLabel = '';
-  myPullUpRefrushLabel = '';
-  $("#pull_up_icon").removeClass();
-  $( ".pullUpLabel" ).html( '' );
-}
-}
-} else {
-  pullUpLabel = '加载失败,请重试';
-}
+  if (data.code == 1) {
+    lastComments = data.result.length;
+    appendReviewsList(data.result);
+    if (lastComments != 15) {
+      if (pageNum != 1) {
+        myPullLoadLabel = '评论已经加载完啦';
+        myPullUpLabel = '评论已经加载完啦';
+        myPullUpRefrushLabel = '评论已经加载完啦';
+        $("#pull_up_icon").removeClass();
+        $(".pullUpLabel").html('评论已经加载完啦');
+      } else {
+        myPullLoadLabel = '';
+        myPullUpLabel = '';
+        myPullUpRefrushLabel = '';
+        $("#pull_up_icon").removeClass();
+        $(".pullUpLabel").html('');
+      }
+    }
+  } else {
+    pullUpLabel = '加载失败,请重试';
+  }
 }
 
-  function appendNewReviewsList () {
+function appendNewReviewsList() {
 
-  if ( lastComments == 15 ) {
-  var formParams = $( "#prodInfoCommentsForm" ).serialize();
-  $.ajax( {
-  type : 'GET',
-  url : 'http://114.55.43.64:8080/o2reviews/product/productinfocomments',
-  data : formParams,
-  dataType : 'jsonp',
-  contentType : 'application/json'
-} );
-} else {
-  if (pageNum != 1) {
-  myPullLoadLabel = '评论已经加载完啦';
-  myPullUpLabel = '评论已经加载完啦';
-  myPullUpRefrushLabel = '评论已经加载完啦';
-  $("#pull_up_icon").removeClass();
-  $( ".pullUpLabel" ).html( '评论已经加载完啦' );
-} else {
-  myPullLoadLabel = '';
-  myPullUpLabel = '';
-  myPullUpRefrushLabel = '';
-  $("#pull_up_icon").removeClass();
-  $( ".pullUpLabel" ).html( '' );
-}
-}
+  if (lastComments == 15) {
+    var formParams = $("#prodInfoCommentsForm").serialize();
+    $.ajax({
+      type: 'GET',
+      url: 'http://114.55.43.64:8080/o2reviews/product/productinfocomments',
+      data: formParams,
+      dataType: 'jsonp',
+      contentType: 'application/json'
+    });
+  } else {
+    if (pageNum != 1) {
+      myPullLoadLabel = '评论已经加载完啦';
+      myPullUpLabel = '评论已经加载完啦';
+      myPullUpRefrushLabel = '评论已经加载完啦';
+      $("#pull_up_icon").removeClass();
+      $(".pullUpLabel").html('评论已经加载完啦');
+    } else {
+      myPullLoadLabel = '';
+      myPullUpLabel = '';
+      myPullUpRefrushLabel = '';
+      $("#pull_up_icon").removeClass();
+      $(".pullUpLabel").html('');
+    }
+  }
 }
